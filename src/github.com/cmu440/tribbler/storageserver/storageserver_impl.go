1a80ab5742ab25d31ece68672f8233781ba8320d
Won Gu Kang <kangwo1@gmail.com>
2014-03-31 02:39:44 -0400

    Fixed syntax for sort

--- src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
+++ src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
@@ -15,9 +15,9 @@ import (
 
 type Nodes []storagerpc.Node
 
-func (n Nodes) Len() int            { return len(n) }
-func (n Nodes) Swap(i, j, int)      { n[i], n[j] = n[j], n[i] }
-func (n Nodes) Less(i, j, int) bool { return n[i].NodeID < n[j].NodeID }
+func (n Nodes) Len() int           { return len(n) }
+func (n Nodes) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+func (n Nodes) Less(i, j int) bool { return n[i].NodeID < n[j].NodeID }
 
 type storageServer struct {
 	master       bool
